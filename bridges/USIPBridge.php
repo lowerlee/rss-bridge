ae1de34bea2291dcf554d4511eb7a9a59f5b5daa
lowerlee <minleekevin@gmail.com>
2024-12-30 14:21:40 -0800

    made tester USIP script

--- bridges/USIPBridge.php
+++ bridges/USIPBridge.php
@@ -1,25 +1,49 @@
 <?php
 
-class USIPBridge extends BridgeAbstract
+class ABCNewsBridge extends BridgeAbstract
 {
-    const NAME = 'USIP Publications Bridge';
-    const URI = 'https://www.usip.org/publications';
-    const DESCRIPTION = 'Returns titles from USIP Publications';
-    const MAINTAINER = 'Your Name';
-    const CACHE_TIMEOUT = 3600;
+    const NAME = 'ABC News Bridge';
+    const URI = 'https://www.abc.net.au';
+    const DESCRIPTION = 'Topics of the Australian Broadcasting Corporation';
+    const MAINTAINER = 'yue-dongchen';
 
-    public function collectData() {
-        $html = getSimpleHTMLDOM(self::URI);
-        $articles = $html->find('article.summary');
+    const PARAMETERS = [
+        [
+            'topic' => [
+                'type' => 'list',
+                'name' => 'Region',
+                'title' => 'Choose state',
+                'values' => [
+                    'ACT' => 'act',
+                    'NSW' => 'nsw',
+                    'NT' => 'nt',
+                    'QLD' => 'qld',
+                    'SA' => 'sa',
+                    'TAS' => 'tas',
+                    'VIC' => 'vic',
+                    'WA' => 'wa'
+                ],
+            ]
+        ]
+    ];
 
-        foreach($articles as $article) {
-            $item = [];
-            $titleElement = $article->find('h3.summary__heading a', 0);
-            if ($titleElement) {
-                $item['title'] = $titleElement->plaintext;
-                $item['uri'] = 'https://www.usip.org' . $titleElement->href;
-            }
-            $this->items[] = $item;
+    public function collectData()
+    {
+        $url = sprintf('https://www.abc.net.au/news/%s', $this->getInput('topic'));
+        $dom = getSimpleHTMLDOM($url);
+        $dom = $dom->find('div[data-component="PaginationList"]', 0);
+        if (!$dom) {
+            throw new \Exception(sprintf('Unable to find css selector on `%s`', $url));
+        }
+        $dom = defaultLinkTo($dom, $this->getURI());
+        foreach ($dom->find('article[data-component="DetailCard"]') as $article) {
+            $a = $article->find('a', 0);
+            $this->items[] = [
+                'title' => $a->plaintext,
+                'uri' => $a->href,
+                'content' => $article->find('p', 0)->plaintext,
+                'timestamp' => strtotime($article->find('time', 0)->datetime),
+            ];
         }
     }
 }
