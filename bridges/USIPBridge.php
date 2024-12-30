7e49b121da10934ad3392b3895d227bc88c54139
lowerlee <minleekevin@gmail.com>
2024-12-30 15:26:07 -0800

    initial USIP bridge script

--- bridges/USIPBridge.php
+++ bridges/USIPBridge.php
@@ -1,49 +1,82 @@
 <?php
 
-class ABCNewsBridge extends BridgeAbstract
+class USIPBridge extends BridgeAbstract
 {
-    const NAME = 'ABC News Bridge';
-    const URI = 'https://www.abc.net.au';
-    const DESCRIPTION = 'Topics of the Australian Broadcasting Corporation';
-    const MAINTAINER = 'yue-dongchen';
-
+    const NAME = 'USIP Publications Bridge';
+    const URI = 'https://www.usip.org/publications';
+    const DESCRIPTION = 'Returns latest publications from United States Institute of Peace';
+    const MAINTAINER = 'Your Name';
+    const CACHE_TIMEOUT = 3600; // 1 hour
+    
     const PARAMETERS = [
-        [
-            'topic' => [
+        'Publication Type' => [
+            'type' => [
+                'name' => 'Type',
                 'type' => 'list',
-                'name' => 'Region',
-                'title' => 'Choose state',
                 'values' => [
-                    'ACT' => 'act',
-                    'NSW' => 'nsw',
-                    'NT' => 'nt',
-                    'QLD' => 'qld',
-                    'SA' => 'sa',
-                    'TAS' => 'tas',
-                    'VIC' => 'vic',
-                    'WA' => 'wa'
-                ],
+                    'Analysis' => '12',
+                    'Report' => '7',
+                    'Article' => '4',
+                    'Discussion Paper' => '19121'
+                ]
             ]
         ]
     ];
 
     public function collectData()
     {
-        $url = sprintf('https://www.abc.net.au/news/%s', $this->getInput('topic'));
-        $dom = getSimpleHTMLDOM($url);
-        $dom = $dom->find('div[data-component="PaginationList"]', 0);
-        if (!$dom) {
-            throw new \Exception(sprintf('Unable to find css selector on `%s`', $url));
+        $url = self::URI;
+        if ($this->getInput('type')) {
+            $url .= '?publication_type%5B0%5D=' . $this->getInput('type');
         }
-        $dom = defaultLinkTo($dom, $this->getURI());
-        foreach ($dom->find('article[data-component="DetailCard"]') as $article) {
-            $a = $article->find('a', 0);
-            $this->items[] = [
-                'title' => $a->plaintext,
-                'uri' => $a->href,
-                'content' => $article->find('p', 0)->plaintext,
-                'timestamp' => strtotime($article->find('time', 0)->datetime),
-            ];
+
+        $html = getSimpleHTMLDOM($url);
+
+        $articles = $html->find('article.summary');
+
+        foreach ($articles as $article) {
+            $item = [];
+
+            // Get title and URL
+            $titleElement = $article->find('h3.summary__heading a', 0);
+            $item['uri'] = 'https://www.usip.org' . $titleElement->href;
+            $item['title'] = $titleElement->plaintext;
+
+            // Get date
+            $dateElement = $article->find('span.published-date', 0);
+            if ($dateElement) {
+                $item['timestamp'] = strtotime($dateElement->plaintext);
+            }
+
+            // Get author
+            $authorElement = $article->find('p.meta.publication-by-line a', 0);
+            if ($authorElement) {
+                $item['author'] = $authorElement->plaintext;
+            }
+
+            // Get content
+            $contentElement = $article->find('div.summary__text p', 0);
+            if ($contentElement) {
+                $item['content'] = $contentElement->plaintext;
+            }
+
+            // Get image
+            $imageElement = $article->find('img', 0);
+            if ($imageElement) {
+                $item['enclosures'] = [$imageElement->src];
+            }
+
+            $this->items[] = $item;
         }
+    }
+
+    public function getName()
+    {
+        if (!is_null($this->getInput('type'))) {
+            $types = $this->getParameters()['Publication Type']['type']['values'];
+            $typeName = array_search($this->getInput('type'), $types);
+            return 'USIP Publications - ' . $typeName;
+        }
+        return parent::getName();
     }
 }
